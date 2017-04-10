9d5fb799d79acc6fedba977917318edbda30491e
DEPIDSVY <depidsvy@nintendo.de>
2017-04-10 11:29:51 +0200

    - FIX downloading data in parallel by injecting Guzzle\Client

--- src/AsyncGithub.php
+++ src/AsyncGithub.php
@@ -3,7 +3,13 @@
 namespace GitGuzzle;
 
 use Github\Api\Repo;
-use Github\Client;
+use Github\Client as GithubClient;
+use Guzzle\Http\Message\RequestFactory;
+use GuzzleHttp\Client as GuzzleClient;
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Pool;
+use GuzzleHttp\Psr7\Response;
+use Http\Adapter\Guzzle6\Client as AdapterClient;
 use Http\Client\Common\HttpMethodsClient;
 use Http\Discovery\MessageFactoryDiscovery;
 
@@ -12,7 +18,7 @@ class AsyncGithub {
 	public $username = 'ornicar';
 
 	/**
-	 * @var Client
+	 * @var GithubClient
 	 */
 	protected $client;
 
@@ -21,15 +27,63 @@ class AsyncGithub {
 	 */
 	protected $httpClient;
 
+	var $base = 'http://localhost:8081/slawa/guzzle/htdocs/';
+
 	function __construct() {
-		$this->client = new Client(null, null, 'http://localhost:8081/slawa/guzzle/htdocs/');
-		$this->httpClient = $this->client->getHttpClient();
+		//$this->client = new GithubClient(null, null, $this->base);
+		//$this->httpClient = $this->client->getHttpClient();
+		$this->httpClient = new GuzzleClient([
+			'base_uri' => $this->base,
+			'proxy' => '',
+		]);
+
+		//$hc = $this->httpClient;
+		$hc = new AdapterClient($this->httpClient);
+
+		$this->client = GithubClient::createWithHttpClient(
+			new	HttpMethodsClient($hc,
+				MessageFactoryDiscovery::find()
+			)
+		);
 	}
 
 	function render() {
 		/** @var \Github\Api\User $user */
 		//$user = $this->client->api('user');
 		//$repositories = $user->repositories($this->username);
+		$repositories = $this->getFakeRepos();
+		echo 'Repos: ', sizeof($repositories), BR;
+
+		$requests = $this->generateRequests($repositories);
+
+		$pool = new Pool($this->httpClient, $requests, [
+			'concurrency' => 5,
+			'fulfilled' => [$this, 'requestFulfilled'],
+			'rejected' => function (ClientException $reason, $index) {
+				// this is delivered each failed request
+				echo $index, TAB, $reason->getMessage(), BR;
+			},
+		]);
+		$promise = $pool->promise();
+
+		$promise->wait();
+		echo 'Done in ',
+		number_format(microtime(true) - $_SERVER['REQUEST_TIME_FLOAT'], 3), BR;
+	}
+
+	function show($username, $repository) {
+		$url = 'sleep.php/repos/'.rawurlencode($username).'/'.rawurlencode($repository);
+
+		$requestFactory = MessageFactoryDiscovery::find();
+		$request = $requestFactory->createRequest('get', $url, [], NULL);
+
+		return $request;
+	}
+
+	/**
+	 * @return array
+	 */
+	private function getFakeRepos(): array {
 		$repositories = [
 			'ag.vim',
 			'ahDoctrineEasyEmbeddedRelationsPlugin',
@@ -65,7 +119,15 @@ class AsyncGithub {
 		$repositories = array_map(function ($name) {
 			return ['name' => $name];
 		}, $repositories);
+		return $repositories;
+	}
 
+	/**
+	 * @param $repositories
+	 * @return array
+	 */
+	private function generateRequests($repositories): array {
+		$requests = [];
 		/** @var Repo $repo */
 //		$repo = $client->api('repo');
 		foreach ($repositories as $rep) {
@@ -73,19 +135,19 @@ class AsyncGithub {
 			$request = $this->show($this->username, $rep['name']);
 			echo TAB, $request->getUri(), BR;
 			//echo TAB, $info['forks'], BR;
+			//break;
+			$requests[] = $request;
 		}
-		var_dump($request);
-		$response = $this->httpClient->sendRequest($request);
-		var_dump($response);
+		return $requests;
+		//var_dump($request);
+//		$response = $this->httpClient->send($request);
+//		var_dump($response);
 	}
 
-	function show($username, $repository) {
-		$url = 'sleep.php/repos/'.rawurlencode($username).'/'.rawurlencode($repository);
-
-		$requestFactory = MessageFactoryDiscovery::find();
-		$request = $requestFactory->createRequest('get', $url, [], NULL);
-
-		return $request;
+	function requestFulfilled(Response $response, $index) {
+		// this is delivered each successful response
+		echo $index, TAB, $response->getStatusCode(), BR;
+		echo $response->getBody()->getContents(), BR;
 	}
 
 }
